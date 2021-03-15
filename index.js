14a63774de8a1af0f655404744ce649b9617c6ef
Neto Pompeu <neto1@hotmail.fr>
2021-03-15 10:18:13 +0100

    Learn Array

---
+++ index.js
@@ -0,0 +1,11 @@
+const array = ['Paris', 'Lyon', 'Montreal', 'Bordeaux'];
+const array1 = ['Paris', 33, ['Montreal', 'Bordeaux'], true]
+
+for (i = 0; i < array1.length; i++) {
+    console.log (typeof array1[i])
+}
+const numbers = [21,45,1,22,44,7]
+numbers.sort();
+console.log(numbers.sort())
+
+// *******************************
