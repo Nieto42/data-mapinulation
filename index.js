4ce95cb600e1a1c58dfd4ddaea60051daf168a78
Neto Pompeu <neto1@hotmail.fr>
2021-03-15 15:53:39 +0100

    Correct Split and Replace

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
-const array = ['Paris', 'Lyon', 'Montreal', 'Bordeaux'];
-const array1 = ['Paris', 33, ['Montreal', 'Bordeaux'], true]
+//// Version 1
+/*
+const input = document.querySelector('.input')
+const video = document.querySelector('.video')
+let link = "";
 
-for (i = 0; i < array1.length; i++) {
-    console.log (typeof array1[i])
+input.addEventListener('input', (e) => {
+  link = changeLink(e.target.value); 
+
+ console.log(link)
+   if (link) {
+       video.innerHTML = `<iframe width="1346" height="694" src="${link}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+    }
+});
+
+const changeLink = (linkToChange) => {
+
+    let embed = linkToChange;
+    
+    embed = embed.replace('watch?v=', 'embed/');
+     console.log(embed);
+
+
+    embed = embed.replace('youtu.be/', 'youtube.com/embed/');
+    return embed.split('?')[0];
+  
 }
-const numbers = [21,45,1,22,44,7]
-numbers.sort();
-console.log(numbers.sort())
+*//
+    
 
-// *******************************
+// https://www.youtube.com/embed/QB1DTl7HFnc
+// https://www.youtube.com/watch?v=QB1DTl7HFnc
+// https://youtu.be/QB1DTl7HFnc?t=5998
+
+// Version 2
+
 const input = document.querySelector('.input')
 const video = document.querySelector('.video')
 let link = "";
@@ -23,18 +48,9 @@ input.addEventListener('input', (e) => {
 
 const changeLink = (linkToChange) => {
 
-    let embed = linkToChange.replace('watch?v=', "embed/");
-    link = embed.split('?')[0];
-
-    let be = linkToChange.replace('https://youtu.be/', 'https://youtube.com/embed/');
-    link = be.split('?'[1])
+    let embed = linkToChange.replace('watch?v=', "embed/"); // //https://www.youtube.com/watch?v=QB1DTl7HFnc
+    link = embed.split('?')[0]; // https://www.youtube.com/embed/QB1DTl7HFnc
+// 
+    let be = link.replace('https://youtu.be/', 'https://youtube.com/embed/'); //https://www.youtube.com/embed/QB1DTl7HFnc
+    link = be.split('?'[0])
 }
-
-
-
-
-
-
-// https://www.youtube.com/embed/QB1DTl7HFnc
-// https://www.youtube.com/watch?v=QB1DTl7HFnc
-// https://youtu.be/QB1DTl7HFnc?t=5998
